1c24a0bd7d0f9d17b7077b055d4b30e6f07cb4ae
Wan11703 <vincentmark111703@gmail.com>
2025-09-13 15:09:06 +0800

    changed from bcrypt to bcryptjs

--- express-app/server.js
+++ express-app/server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const multer = require("multer");
 const path = require("path");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 // const bodyParser = require("body-parser");
 
 const fs = require('fs');
